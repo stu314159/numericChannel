97fd8b2e1df48f8ea2187561262603314c588d88
Stu Blair <stu314159@gmail.com>
2015-07-22 12:50:10 +0000

    updated p_conv_fact in genInput.py and modified process_lbm_data to read the new input file

--- genInput.py
+++ genInput.py
@@ -158,7 +158,7 @@ omega = 1./(3.*nu_lbm+0.5)
 u_conv_fact = (dx/dt)*(Lo/To)
 t_conv_fact = (dt*To)
 l_conv_fact = dx*Lo
-p_conv_fact = ((l_conv_fact/t_conv_fact)**2)*(1./3.)
+p_conv_fact = (((l_conv_fact/t_conv_fact)**2)*(1./3.))/(l_conv_fact**3)
 
 rho_lbm = rho_p*(l_conv_fact**3)
 
